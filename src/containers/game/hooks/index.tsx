cb83680952bca91e0db7442340232643fa69989d
Numan <ahmet.aral@ucalgary.ca>
2022-03-13 17:55:11 -0600

    rename & refactor

--- src/containers/game/hooks/index.tsx
+++ src/containers/game/hooks/index.tsx
@@ -3,6 +3,6 @@ export { default as useCharacterState } from './useCharacterState';
 export { default as useGameActions } from './useGameActions';
 export { default as useGameContext } from './useGameContext';
 export { default as useGameState } from './useGameState';
-export { default as useHandleCharacterStateAndDirection } from './useHandleCharacterStateAndDirection';
+export { default as useHandleCharacterActions } from './useHandleCharacterActions';
 export { default as useLevelState } from './useLevelState';
 export { default as useViewState } from './useViewState';
